0cd268a8079c9df07a889ea066e08433ac575b12
kk-gs1943 <{k.19430425@gmail.com}>
2021-07-06 01:59:54 +0900

    first commit

---
+++ JavaScript/clock.js
@@ -0,0 +1,33 @@
+//時計
+const clock = () => {
+    const d = new Date(); //現在の日付・時刻の取得
+    let year = d.getFullYear(); //年を取得
+    let month = d.getMonth() + 1; //月を取得
+    let date = d.getDate();
+    let dayNum = d.getDay();
+    const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let day = weekday[dayNum];
+    let hour = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds();
+
+    //1桁の場合、0を足して2桁にする
+    month = month < 10 ? "0" + month : month;
+    date = date < 10 ? "0" + date : date;
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    //日付・時刻の文字列を作成
+    let today = `${year}-${month}-${date} ${day}`;
+    let time = `${hour}:${min}`;
+    let seconds = `${sec}`; 
+
+    //文字列を表示する
+    document.querySelector(".clock-date").innerText = today;
+    document.querySelector(".clock-time").innerText = time;
+    document.querySelector(".clock-sec").innerText = seconds;
+}
+
+//1秒ごとにclock関数を呼び出す
+setInterval(clock, 1000);
---
+++ JavaScript/stopwatch.js
@@ -0,0 +1,54 @@
+let startTime; 
+let timeoutId;
+const timer = document.getElementById('timer');
+let elapsedTime = 0;
+const start = document.getElementById('start');
+const timeout = document.getElementById('timeout');
+const reset = document.getElementById('reset');
+
+const countUp = () => {
+    const d = new Date(Date.now() - startTime + elapsedTime); //下記メソッド使用のためにDateクラスのインスタンスを作成
+    const m = String(d.getMinutes()).padStart(2, '0'); //文字列にして指定の桁数を取得、満たない場合０で埋める
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
+    timer.textContent = `${m}:${s}.${ms}`; //時間を表示
+    timeoutId = setTimeout(() => {
+        countUp();
+    }, 10); //10ミリ秒毎に時間を取得
+}
+//startボタンを押したとき
+start.addEventListener('click', () => {
+    startTime = Date.now();
+    countUp();
+});
+//stopボタンを押したときにsetTimeout()が止まる
+timeout.addEventListener('click', () => {
+    clearTimeout(timeoutId);
+    elapsedTime += Date.now() - startTime; //タイマーが走っていた時間 ＝ タイマーが走っていた時間 ＋ 現在時刻 ー startボタンを押した時刻
+});
+//resetボタンを押したとき
+reset.addEventListener('click', () => {
+    timer.textContent = '00:00.000';
+    elapsedTime = 0;
+});
+
+//ボタンの有効・無効の切り替え
+start.addEventListener('click', () => {
+    start.disabled = true;
+    timeout.disabled = false;
+    reset.disabled = true;
+});
+timeout.addEventListener('click', () => {
+    start.disabled = false;
+    timeout.disabled = true;
+    reset.disabled = false;
+});
+reset.addEventListener('click', () => {
+    start.disabled = false;
+    timeout.disabled = true;
+    reset.disabled = true;
+});
+
+
+
+
